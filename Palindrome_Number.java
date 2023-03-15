f35c2edb56a681789f6cc44f37625bfcdc3ac3fe
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-15 18:45:19 +0530

    Get input from user and check number is palindrome or not.

---
+++ Palindrome_Number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=sc.nextInt();
+		int temp = num;
+		int rem,rev=0;
+
+		while(temp!=0)
+		{
+		    rem=temp%10;
+		    rev=rev*10+rem;
+		    temp=temp/10;
+		    
+		}
+		if(num==rev)
+		{
+		    System.out.println("The number is palindrome");
+		    
+		}
+		else
+		{
+		    System.out.println("The number is not palindrome");
+		}
+		
+		
+	}
+}
